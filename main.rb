3c68018e10401565535a8bd4b3a796f0ce55cafa
Khoa Hoang <whoskhoahoang@gmail.com>
2020-05-05 19:18:18 +0900

    Refactor code
    Notable: Convert values when actual sorting
    	 takes place rather than creating
    	 an intermediate variable that con-
    	 tains the converted values.

--- main.rb
+++ main.rb
@@ -8,8 +8,10 @@ num_probs = 0
 PROB_LIM = 5  # Hard-code this for now
 
 
+# TODO: Wrap while loop in a method
 # Use Process, not Time.now. Reference:
 # https://blog.dnsimple.com/2018/03/elapsed-time-with-ruby-the-right-way/
+num_correct = 0
 t1 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 while num_probs < PROB_LIM
   prob = Problem.new
@@ -19,6 +21,7 @@ while num_probs < PROB_LIM
   #       will cause user_ans == prob.ans to be true...
   if user_ans == prob.ans
     puts "GOOD JOB"
+    num_correct+=1
   else
     puts "WRONG. The correct answer was %d" % [prob.ans]
   end
@@ -26,14 +29,13 @@ while num_probs < PROB_LIM
 end
 t2 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 t_elapsed = Time.at(t2-t1).utc.strftime("%H:%M:%S.%2N")
+score = num_correct.to_s+"/"+PROB_LIM.to_s
 puts t_elapsed
+puts score
 
-
-# TODO: Factor in how many problems the user got
-#       correct into the scores placement
 if File.exists? "scores.txt"
-  scores_content = Utils.process_scores(t_elapsed)
+  scores_content = Utils.process_scores(t_elapsed, score)
   File.write("scores.txt", scores_content, mode: "w")
 else
-  File.open("scores.txt", "w") { |f| f.write "1. #{t_elapsed}" }
+  File.open("scores.txt", "w") { |f| f.write "1. #{t_elapsed}, #{score}" }
 end
--- utils.rb
+++ utils.rb
@@ -1,18 +1,61 @@
-module Utils
-  def self.process_scores(t_elapsed)
-    content = File.read("scores.txt").split("\n")
-    times = content.map{ |l| Time.parse(l.split(". ")[1]) }
-    times.append(Time.parse(t_elapsed))
-    times = times.sort
-    if times.length > 3
-      times = times.slice(0, 3)
-    end
-
-    outfile = times.each_with_index
-                   .map{ |t, i| "#{i+1}. #{t.utc.strftime("%H:%M:%S.%2N")}" }
-                   .join("\n")
-  end
-end
-
 # PROTIP: Think of a module as a specific kind of
 #         class that only has static methods/variables
+module Utils
+
+  def self.process_scores(t_elapsed, score)
+    content = File.read("scores.txt").split("\n")
+    entries = self.parse_entries(content)
+    entries.append([t_elapsed, score])
+
+    # TODO: Come up with way to evaluate time and score together?
+    entries = entries.sort_by{ |e| [Time.parse(e[0]), -e[1].to_r]}
+    entries = entries.slice(0, 3) if entries.length > 3
+
+    return self.gen_scores_content(entries)
+  end
+
+
+  def self.parse_entries(content)
+    # Parses the entries in a scores file.
+    # @param content: The string contents of a scores file
+    # type contet: String
+    # return: An array of a score file's parsed contents
+    #         to allow for easier processing.
+    # rtype: [Array]
+    result = []
+    content.each do |line|
+      entry = line.split(". ")[1]
+      comps = entry.split(", ")
+      result.append([comps[0].strip, comps[1].strip])
+    end
+
+    return result
+  end
+
+
+  def self.gen_scores_content(entries)
+    # Generates the string to be written to a scores file.
+    # @param entries: An array containing the entries
+    #                 to be written in a scores file
+    # type entries: Array
+    # return: A string to be written to a scores file
+    # rtype: Array
+    return entries.each_with_index
+                  .map{ |e, i| self.format_entry(i, e)}
+                  .join("\n")
+  end
+
+
+  def self.format_entry(rank, info)
+    # Returns a formatted string containing the information
+    # for an entry scores file
+    # @param rank: A 0-based rank of an entry
+    # @param info: An array containing information
+    #              for an entry where the 0th element
+    #              is the time and the 1st element is the score
+    # return: A formatted string containing the information
+    #         for an entry scores file
+    # rtype: String
+    return "#{rank+1}. #{info[0]}, #{info[1].to_r.to_s}"
+  end
+end
